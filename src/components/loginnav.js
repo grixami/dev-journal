bb960e1c91e2289501e7c500f47151e051e3b4d8
grixami <contactgrixami@gmail.com>
2025-06-29 22:35:32 +0100

    404 error page and other ui fixes

--- src/components/loginnav.js
+++ src/components/loginnav.js
@@ -15,11 +15,11 @@ export default function LoginNav({}) {
 
     return(
       <header className="shadow-md h-20 flex items-center border-b-2 border-[#3d444d] bg-[#010409]">
-        <div className="w-full flex items-center justify-between px-4">
-          <Link href="/">
-          <div className="hidden lg:flex items-center gap-0">
-          <h1 className="font-bold text-3xl px-2 pl-20 ">Dev Journal</h1>
-            <Image
+        <div className="w-full inline-flex items-center justify-between mx-4">
+          <Link href="/" className="transition-transform duration-300 hover:scale-105">
+          <div className=" hidden lg:flex items-center gap-0">
+            <h1 className="font-bold text-3xl mx-2 ml-20 bor ">Dev Journal</h1>
+              <Image
                 className="dark:invert"
                 src="/assets/img/logo.png"
                 alt="logo"
@@ -30,7 +30,7 @@ export default function LoginNav({}) {
           </Link>
           <div className="flex justify-center items-center">
             <form className="flex items-center" action="/search">
-              <input type="text" name="query" className="border focus:border-[#5a9ef9] focus:outline-none rounded-lg h-10 resize-none py-2 px-2 overflow-hidden" placeholder="search..."></input>
+              <input type="text" name="query" className="border focus:border-[#5a9ef9] focus:outline-none rounded-lg h-10 resize-none py-2 px-2 overflow-hidden transition-transform duration-300 focus:scale-105 hover:scale-105" placeholder="search..."></input>
               <button type="submit">
               <Image
                   className="cursor-pointer align-middle dark:invert mx-3  border border-black rounded-full py-1 px-1 bg-[#fefbf6] hover:bg-[#9e9c95] duration-300 transition-transform ease-in-out hover:scale-110 hover:rotate-30"
